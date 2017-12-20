1a87be844453652b32671930bbecfd48b303967d
Allan Tabri <allan.tabri@mail.ee>
2017-12-20 12:55:48 +0200

    Typescript, formi lisamine

--- Projekt_4/app/main.ts
+++ Projekt_4/app/main.ts
@@ -1,6 +1,8 @@
 /// <reference path='helper.ts'/>
 /// <reference path='navigation.ts'/>
-/// <reference path='Page.ts'/>
+/// <reference path='page.ts'/>
+/// <reference path='home.ts'/>
+/// <reference path='eventPage.ts'/>
 console.log('main.ts');
 class App {
     private _navLinks: INavLink[] = [{name:'Pealeht', link: '#home'},
@@ -8,18 +10,21 @@ class App {
                                     {name:'Üritus', link: '#event'}];
     
     private _page: Page;
+
     constructor() {
         this._bindEvents();
         this._setup();
+        this._urlChanged();
     }
     private _bindEvents(){
-
+        window.addEventListener('hashchange', this._urlChanged.bind(this));
     }
     private _setup(){
         if (window.location.hash === '') {
             window.location.hash = this._navLinks[0].link;
         }   
         const nav = new Navigation(this._navLinks);
+        this._checkParams();
         this._urlChanged();
     }
     private _urlChanged(){
@@ -29,14 +34,27 @@ class App {
             if(window.location.hash === value.link) {
                 if (value.link === this._navLinks[0].link){
                     this._page = new Home();
-                }
+                } else if (value.link === this._navLinks[2].link){
+                    this._page = new EventPage();
             }
         }
-    )
+    );
+
     }
     private _checkParams(){
-
+        const name = Helper.getParameterByName('name');
+        const joined = Helper.getParameterByName('joined');
+        if (name && joined) {
+            Helper.removeParams();
+            let people: IParticipant[] = JSON.parse(localStorage.getItem('people'));
+            if (!people) {
+                people = [];
+            }
+            const person: IParticipant = {name, joined}; // {name: name, joined: joined}
+            people.push(person);
+            console.log(people);
+            localStorage.setItem('people', JSON.stringify(people));
+        }
     }
-
 }
 let app = new App();
